ec5f9a4dd1e9e4568f700130afdc6b2772c696db
marco <lazzari.marco.ml@gmail.com>
2024-03-07 17:06:08 +0100

    stilizzato header

--- src/components/AppNav.vue
+++ src/components/AppNav.vue
@@ -12,19 +12,29 @@
 </script>
 
 <template>
-    <nav class="bg-danger-subtle p-3 d-flex justify-content-between align-items-center">
-        <h1 class="text-danger">
+    <nav class=" d-flex justify-content-between align-items-stretch mb-3">
+        <h1 class="text-danger p-3 m-0">
             BOOLFLIX
         </h1>
 
-        <div class="d-flex gap-3">
-            <input type="search" v-model="store.search">
-            <button class="btn btn-outline-light" @click="$emit('search')">Search</button>
+        <div class="d-flex gap-3 flex-grow-1">
+            <input type="search" v-model="store.search" class="search_bar flex-grow-1 bg-transparent border-0 fs-1 px-3" placeholder="Search for something to watch...">
+            <button class="search_bar btn btn-lg rounded-0 fs-2" @click="$emit('search')">Search</button>
         </div>
             
     </nav>
 </template>
 
 <style lang="scss">
+    .search_bar:focus-visible{
+        outline: none;
+    }
+
+    nav{
+        background-image: linear-gradient(black, transparent);
+    }
+
+
+
 
 </style>
