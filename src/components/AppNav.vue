002d09d41a9cff7682763a9a10341216b054237e
marco <lazzari.marco.ml@gmail.com>
2024-03-08 10:41:14 +0100

    fix: corretto stilizzazioni navbar che causavano la rottura del layout a basse risoluzioni

--- src/components/AppNav.vue
+++ src/components/AppNav.vue
@@ -14,25 +14,27 @@
 <template>
     <nav class=" d-flex justify-content-between align-items-stretch mb-3">
         <h1 class="text-danger p-3 m-0">
-            BOOLFLIX
+            B<span class="d-none d-sm-inline">OOL</span>F<span class="d-none d-sm-inline">LIX</span>
         </h1>
 
-        <div class="d-flex gap-3 flex-grow-1">
-            <input type="search" v-model="store.search" class="search_bar flex-grow-1 bg-transparent border-0 fs-1 px-3" placeholder="Search for something to watch...">
-            <button class="search_bar btn btn-lg rounded-0 fs-2" @click="$emit('search')">Search</button>
+        <div class="d-flex flex-grow-1">
+            <input type="search" v-model="store.search" class="search_bar flex-grow-1 bg-transparent border-0 fs-1 ps-3 w-100" placeholder="Search for something to watch...">
+            <button class="btn btn-lg rounded-0 fs-2" @click="$emit('search')">Search</button>
         </div>
             
     </nav>
 </template>
 
 <style lang="scss">
-    .search_bar:focus-visible{
+
+.search_bar:focus-visible{
         outline: none;
     }
 
-    nav{
-        background-image: linear-gradient(black, transparent);
-    }
+nav{
+    background-image: linear-gradient(black, transparent);
+}
+
 
 
 
